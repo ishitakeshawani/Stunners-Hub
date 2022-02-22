860b9a76eee2c0b64b35537ff8bdf37aec0af19e
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-02-22 14:02:03 +0530

    feat:added search page

--- pages/home/mobile-menu.js
+++ pages/home/mobile-menu.js
@@ -1,9 +1,11 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu =  document.querySelector('.openMenu');
+const searchInput = document.querySelector('.search-input');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
+searchInput.addEventListener('click',searchItem);
 
 function show(){
     mainMenu.style.display = 'flex';
@@ -12,4 +14,9 @@ function show(){
 
 function close(){
     mainMenu.style.top = '-100%';
+}
+
+function searchItem(){
+    console.log("hugf");
+    location.href = "/pages/Search/search.html";
 }
