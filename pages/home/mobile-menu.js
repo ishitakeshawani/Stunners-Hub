d9ebe234ba3ab1f831915ec233ceba2f9b265480
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-02-22 18:27:15 +0530

    Revert "added search page"

--- pages/home/mobile-menu.js
+++ pages/home/mobile-menu.js
@@ -1,11 +1,9 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu =  document.querySelector('.openMenu');
-const searchInput = document.querySelector('.search-input');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
-searchInput.addEventListener('click',searchItem);
 
 function show(){
     mainMenu.style.display = 'flex';
@@ -14,8 +12,4 @@ function show(){
 
 function close(){
     mainMenu.style.top = '-100%';
-}
-
-function searchItem(){
-    location.href = "/pages/Search/search.html";
 }
